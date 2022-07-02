e1d5e1a8455982f355aabc08cd73a3ea8db41fcd
lmberard <lberard@fi.uba.ar>
2022-07-02 16:26:28 -0300

    cleaning code

--- src/app.js
+++ src/app.js
@@ -1,9 +1,3 @@
-// import '../node_modules/@metamask/legacy-web3'
-// // // v ar contract = require("truffle-contract");
-// // const { web3 } = window
-// // // const selectedAddress = web3.eth.defaultAccount
-// // const PORT = 7545;
-
 App = {
     contracts: {},
 
@@ -12,11 +6,8 @@ App = {
         await App.loadAccount()
         await App.loadContract()
         web3.eth.defaultAccount = web3.eth.accounts[0]
-        // await App.render()
       },
     
-      
-    // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
     loadWeb3: async () => {
       if (typeof web3 !== 'undefined') {
         App.web3Provider = web3.currentProvider
@@ -24,34 +15,10 @@ App = {
       } else {
         window.alert("Please connect to Metamask.")
       }
-      // Modern dapp browsers...
-      if (window.ethereum) {
-        window.web3 = new Web3(ethereum)
-        try {
-          // Request account access if needed
-          await ethereum.enable()
-          // Acccounts now exposed
-          web3.eth.sendTransaction({/* ... */})
-        } catch (error) {
-          // User denied account access...
-        }
-      }
-      // Legacy dapp browsers...
-      else if (window.web3) {
-        App.web3Provider = web3.currentProvider
-        window.web3 = new Web3(web3.currentProvider)
-        // Acccounts always exposed
-        web3.eth.sendTransaction({/* ... */})
-      }
-      // Non-dapp browsers...
-      else {
-        console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
-      }
     },
 
   loadAccount: async () => {
     // Set the current blockchain account
-    //App.account = web3.eth.accounts[0]
     const accounts = await web3.eth.getAccounts();
     console.log("Account: "+accounts[0]);
     App.account = accounts[0];
@@ -92,32 +59,11 @@ App = {
     card = await App.bingo.seeCard(0, { from: App.account })
     console.log("card: "+ card.toString())
 
-    // const n = new Date().getTime() % 10
-    // const range = 10
-    // const win = await App.bingo.makeMove(n, range, { from: App.account })
-
-     const n = new Date().getTime() % 10
-     const randomNumber = await App.bingo.generateRandom(n, 10)
+    const n = new Date().getTime() % 10
+    const randomNumber = await App.bingo.generateRandom(n, 10)
     var amountMatches = 0;
-    $('.bigNumberDisplay').text(randomNumber)
-    
+    $('.bigNumberDisplay').text(randomNumber)   
     $('td.cell' + randomNumber).addClass('selected')
-    CardMatch = document.getElementById('td.cell'  + randomNumber).getText();
-
-    console.log(CardMatch)
-
-    // if($('td.cell' + randomNumber).addClass('selected')){
-    //   amountMatches++;
-    //   console.log("amount: "+amountMatches);
-    //   $('#amountMatches').text(amountMatches);
-    // }
-    
-      
-
-    console.log("randomNumber: "+ randomNumber.toString())
-
-    //await checkHit();
-
   },
 
   generateRandomRange: async (range) => {
@@ -125,8 +71,6 @@ App = {
   }
   //TODO: hacer una random de numeros dentro de 1 rango
 }
-
-// App.listen(PORT, () => console.log(`app listening at http://localhost:${PORT}`))
 
 $(() => {
   $(window).load(() => {
