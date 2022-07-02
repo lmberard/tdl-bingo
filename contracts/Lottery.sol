859a9d125eb2f7217cc24bd7223e8a336db74a82
lmberard <lberard@fi.uba.ar>
2022-07-02 17:47:47 -0300

    cleaning code and added info

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,13 +1,10 @@
-// SPDX-License-Identifier: GPL-3.0
-// pragma solidity ^0.8.13;
-// pragma solidity ^0.4.17;
 pragma solidity >=0.5.16;
 
 pragma experimental ABIEncoderV2;
 
 contract Lottery {
+    // ATRIBUTES -----------------------------------------------
     uint256 constant NUMBERS_PER_CARD = 2;
-
     struct Participant {
         string name;
         uint256[NUMBERS_PER_CARD] card;
@@ -15,37 +12,30 @@ contract Lottery {
         uint256 amountHits;
     }
     Participant[] private participants;
-
     address private owner;
     address private winner;
     uint256 private roundNumber;
     int256 private lastNumber;
-
     bool private activeGame;
 
-    //uint256 private premio;
-
-    //payable
-    //constructor(uint256 _numerowinnder, uint256 _premio) payable {
+    // CONSTRUCTORS - GETTERS - SETTERS ------------------------
     constructor() public payable {
         owner = msg.sender;
         roundNumber = 0;
         lastNumber = -1;
-        activeGame = true; //para activar o desactivar el juego
+        activeGame = true;
+    }
+
+    function seeLastNumber() public view returns (int256) {
+        return lastNumber;
     }
 
     function checkHit(uint256 _num) public view returns (bool) {
         return lastNumber == int256(_num);
     }
 
-    //"Random function" realmente es seudorandom(?)
-    //private porque es una funcion que solo puede ser accedida dentro del contrato
+    // TODO move to Library
     function rand(uint256 n, uint256 range) private view returns (uint256) {
-        //primero se castea a uint256
-        //keccak256 es un algoritmo de hash (criptografia unidireccional)
-        //now es el tiempo de minado, msg sender es la direccion de la persona que lo ejecuta
-        // los algoritmos de hash siempre son diferentes por eso se genera un numero pseudo aleatorio.
-        // esto genera un valor entero y luego lo castea a uint256
         return
             uint256(
                 keccak256(
@@ -54,10 +44,7 @@ contract Lottery {
             ) % range;
     }
 
-    function seeLastNumber() public view returns (int256) {
-        return lastNumber;
-    }
-
+    // GAME LOGIC ----------------------------------------------
     function checkWinnder() private view returns (bool) {
         for (uint256 i; i < participants.length; i++) {
             if (participants[i].amountHits == NUMBERS_PER_CARD) {
@@ -86,7 +73,7 @@ contract Lottery {
             Participant memory part = participants[i];
             for (uint256 j = 0; j < part.card.length; j++) {
                 if (part.card[j] == lastHit) {
-                        return true;
+                    return true;
                 }
             }
         }
@@ -111,15 +98,23 @@ contract Lottery {
         participants.push(part);
     }
 
-    function seeCard(uint256 _id) public view returns (uint256[NUMBERS_PER_CARD] memory) {
-        if(_id <= participants.length -1){
+    function seeCard(uint256 _id)
+        public
+        view
+        returns (uint256[NUMBERS_PER_CARD] memory)
+    {
+        if (_id <= participants.length - 1) {
             Participant memory part = participants[_id];
             return part.card;
         }
-        return [uint256(0),0];
+        return [uint256(0), 0];
     }
 
-    function generateRandom(uint256 _n, uint256 _range) public view returns (uint256) {
+    function generateRandom(uint256 _n, uint256 _range)
+        public
+        view
+        returns (uint256)
+    {
         return uint256(rand(_n, _range));
     }
 
--- contracts/TodoList.sol
+++
@@ -1,7 +0,0 @@
-//pragma solidity ^0.5.0;
-// pragma solidity ^0.4.17;
-pragma solidity >=0.5.16;
-
-contract TodoList {
-    uint256 taskCount = 0;
-}
